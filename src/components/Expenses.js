d8de26038b9fa6e9d9d76d94df8bf2983cb00abe
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-07-18 17:10:22 -0500

    Refactor and composition with child props

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,16 +1,33 @@
-import ExpenseItem from "./ExpenseItem";
+
+import ExpenseItem from './ExpenseItem';
+import Card from './Card';
+import './Expenses.css';
 
 function Expenses(props) {
-
-    const title = props.item.title;
-    const amount = props.item.amount;
-    const date = props.item.date;
-
-    return (
-
-        <ExpenseItem title={title} date={date} amount={amount} />
-
-    );
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      />
+      <ExpenseItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      />
+      <ExpenseItem
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      />
+      <ExpenseItem
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      />
+    </Card>
+  );
 }
 
 export default Expenses;
