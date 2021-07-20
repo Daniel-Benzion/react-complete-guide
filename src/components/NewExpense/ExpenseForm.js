ad241db3f9fe7e683709d4d3d21d01a5a066a0f6
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-07-20 09:46:02 -0500

    Handling form submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -46,8 +46,19 @@ const ExpenseForm = () => {
     setEnteredDate(event.target.value);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    }
+    console.log(expenseData);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
 
       <div className="new-expense__controls">
 
